dc78beec8547151131997e28f8509965de714459
cibernox <miguel.camba@gmail.com>
2017-08-23 16:40:51 +0200

    Improve helper function

--- test/unit/append-to-content-test.js
+++ test/unit/append-to-content-test.js
@@ -2,19 +2,43 @@
 
 const expect = require('chai').expect;
 const processTemplate = require('../helpers/process-template');
-const syntax = require('@glimmer/syntax');
-const b = syntax.builders;
+const { builders: b } = require('@glimmer/syntax');
+const appendToContent = require('../../lib/helpers/append-to-content');
 
 describe('Helper #appendToContent', function() {
-  describe('it works', function() {
-    let modifiedTemplate = processTemplate(`{{one}}{{two}}`, {
-      MustacheStatement(node) {
-        if (node.path.original === 'one') {
-          return b.mustache(b.path('newContent'));
-        }
-        return node;
+  describe('it can append regular strings to TextNode', function() {
+    let modifiedTemplate = processTemplate(`<div class="foo"></div>`, {
+      AttrNode(attr) {
+        attr.value = appendToContent(b, 'bar', attr.value);
+        attr.value = appendToContent(b, 'baz', attr.value);
+        return attr;
       }
     });
-    expect(modifiedTemplate).to.equal(`{{newContent}}{{two}}`); // how to convert the glimmer AST back to hbs syntax? ASTexplorer.net does it.g
+
+    expect(modifiedTemplate).to.equal(`<div class="foo bar baz"></div>`);
+  });
+
+  describe('it can append TextNodes to another TextNode', function() {
+    let modifiedTemplate = processTemplate(`<div class="foo"></div>`, {
+      AttrNode(attr) {
+        attr.value = appendToContent(b, b.text('bar'), attr.value);
+        attr.value = appendToContent(b, b.text('baz'), attr.value);
+        return attr;
+      }
+    });
+
+    expect(modifiedTemplate).to.equal(`<div class="foo bar baz"></div>`);
+  });
+
+  describe('it can append PathExpression to TextNode', function() {
+    let modifiedTemplate = processTemplate(`<div class="foo"></div>`, {
+      AttrNode(attr) {
+        attr.value = appendToContent(b, b.path('bar'), attr.value);
+        attr.value = appendToContent(b, b.path('baz'), attr.value);
+        return attr;
+      }
+    });
+
+    expect(modifiedTemplate).to.equal(`<div class="foo {{bar}} {{baz}}"></div>`);
   });
 });
